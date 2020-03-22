d3ce17627ced60caccdfdc4b20baf5e6d51629f9
刘帅 <ls_vvv@163.com>
2020-03-23 00:48:54 +0800

    删除示例应用。

--- example/lib/domain/constants/application_constants.dart
+++
@@ -1,5 +0,0 @@
-const String keyApplicationName = '';
-const String keyParseApplicationId = '';
-const String keyParseMasterKey = '';
-const String keyParseServerUrl = '';
-const bool keyDebug = true;
